508492ffc71a46dac1fec4cd16be0211a0e46da7
theCock <Cock@gmail.com>
2022-08-19 14:01:07 +0300

    huhuhuhh

---
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -0,0 +1,57 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.Conveyor.Conveyor;
+import frc.robot.subsystems.Flap.Flap;
+import frc.robot.subsystems.Hood.Hood;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.shooter.Shooter;
+
+public class Superstructure extends SubsystemBase {
+    private final Intake intake = Intake.getInstance();
+    private final Conveyor conveyor = Conveyor.getInstance();
+    private final Shooter shooter = Shooter.getInstance();
+    private final Flap flap = Flap.getInstance();
+    private final Hood hood = Hood.getInstance();
+
+    public void idle() {
+        intake.setPower(0);
+        conveyor.setPower(0);
+        shooter.setPower(0);
+    }
+    public void feed(){
+        intake.openREEEtractor();
+        intake.setPower(0.5);
+    }
+    public void convey(){
+        flap.ShallNotPass();
+        conveyor.setPower(0.5);
+    }
+    public void shoot(){
+        flap.ShallPass();
+        shooter.setPower(0.5);
+    }
+
+    
+
+    public void feedAndConvey() {
+        shooter.setPower(0);
+        intake.openREEEtractor();
+        flap.ShallNotPass();
+        intake.setPower(0.5);
+        conveyor.setPower(0.5);
+    }
+
+    public void warmUp() {
+        intake.setPower(0);
+        conveyor.setPower(0);
+        flap.ShallNotPass();
+        hood.changeAngleForDistance();
+        shooter.setVelocity(shooter.returnSpeedForDistance());
+    }
+}
+
+
+
+
+
